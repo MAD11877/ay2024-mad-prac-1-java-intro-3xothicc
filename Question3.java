1fe7a0eeffb2d9637bb24ef67f63be3c8e97ae9f
Jun Kai <148022731+3xothicc@users.noreply.github.com>
2024-04-16 10:45:46 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Question3
 {
   public static void main(String[] args)
-  {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
+  { 
     Scanner in = new Scanner(System.in);
-    
+    //enter integer
+    System.out.println("Enter an integer:");
+
+    //read input
+    int number = in.nextInt();
+
+    //multiply integer
+    int squared = number * number;
+
+    System.out.println(number + " multiplied by itself is " + squared);
+
+    in.close();
   }
 }
